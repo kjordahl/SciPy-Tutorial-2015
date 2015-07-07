81a1b9ae805d3a85b556295a843c5949234991a6
ocefpaf <ocefpaf@gmail.com>
2015-07-07 12:13:45 -0300

    Simpler conda-setup.sh

--- conda-setup.sh
+++ conda-setup.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
+
 ENV_NAME=2015-geospatial
-conda create --yes -n $ENV_NAME python=2  fiona shapely rasterio pandas basemap requests
-conda install --yes -n $ENV_NAME -c ocefpaf rtree
-conda install --yes -n $ENV_NAME -c asmeurer pyproj descartes
-source activate $ENV_NAME
-pip install git+git://github.com/geopandas/geopandas.git
-pip install github3.py
-pip install --no-deps geojsonio  # Use --no-peds to avoid downgrading six and github3.py
+
+conda config --add channels ioos
+
+conda create -n $ENV_NAME --file req.txt python=2.7
